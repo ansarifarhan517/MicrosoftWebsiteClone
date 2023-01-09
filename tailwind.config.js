c4795426328142286114d37531a1eb3c7e3bae50
farhan@gl3d.in <farhan@gl3d.in>
2023-01-09 18:50:15 +0530

    navbar done

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        'custom-blue': 'rgb(59 130 246 / 0.5)',
+      },
+      fontFamily:{
+        'font-ms': '"Segoe UI",SegoeUI,"Helvetica Neue",Helvetica,Arial,sans-serif'
+      }
+    },
+  },
+  plugins: [],
+}
